45de35bd0f0bb9cbd61377d68debde95019987ca
Andrew Low <andrew.low@inspection.gc.ca>
2018-06-12 15:12:25 -0400

    Table columns now resizable for seqdata. Can add to other tables as necessary

--- olc_database/data_wrapper/tables.py
+++ olc_database/data_wrapper/tables.py
@@ -6,32 +6,32 @@ class SeqDataTable(tables.Table):
     class Meta:
         model = SeqData
         attrs = {'id': 'seqdata-table',
-                 'class': 'table table-hover table-border compact table-sm table-responsive'}
+                 'class': 'table table-hover compact'}
 
 
 class ResFinderDataTable(tables.Table):
     class Meta:
         model = ResFinderData
         attrs = {'id': 'resfinderdata-table',
-                 'class': 'table table-hover table-border compact'}
+                 'class': 'table table-hover compact'}
 
 
 class SeqTrackingTable(tables.Table):
     class Meta:
         model = SeqTracking
         attrs = {'id': 'seqtracking-table',
-                 'class': 'table table-hover table-border compact'}
+                 'class': 'table table-hover compact'}
 
 
 class OLNTable(tables.Table):
     class Meta:
         model = OLN
         attrs = {'id': 'oln-table',
-                 'class': 'table table-hover table-border compact'}
+                 'class': 'table table-hover compact'}
 
 
 class CultureDataTable(tables.Table):
     class Meta:
         model = CultureData
         attrs = {'id': 'culturedata-table',
-                 'class': 'table table-hover table-border compact'}
+                 'class': 'table table-hover compact'}
